432c0738698185d07e08f7dea504bdee59f19f3f
meiregoncalves <30304639+meiregoncalves@users.noreply.github.com>
2017-11-12 19:38:37 -0200

    Menu

--- src/models/categoria.ts
+++
@@ -1,9 +0,0 @@
-export class Categoria {
-  nome:string;
-  id: number;
-  selecionado: boolean;
-  constructor(nome:string, selecionado:boolean) {
-    this.nome = nome;
-    this.selecionado = selecionado;
-  }
-}
