eb3a9f1d781dceb75adb0472c396f8f7b81601cb
daniloclementeb <danilo.clementeb@gmail.com>
2017-11-19 15:16:56 -0200

    inicializa feed de noticias

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -63,9 +63,7 @@ export class HomePage {
     this.listaSites.push(s);
     s = new Site("G1", true, 3);
     this.listaSites.push(s);
-    s = new Site("Terra", true, 4);
-    this.listaSites.push(s);
-    s = new Site("Uol", true, 5);
+    s = new Site("Uol", true, 4);
     this.listaSites.push(s);
     localStorage.set("sites", JSON.stringify(this.listaSites));
   }
@@ -90,13 +88,81 @@ export class HomePage {
 
   inicializaCadastroFeeds(localStorage: LocalStorageService) {
     this.listaCadastro_Feed = [];
+    //Beleza Cultura Economia Educacao Esporte Tecnologia
+    this.feedEstadao("http://www.estadao.com.br/rss/");
 
-    var c = new Cadastro_Feed("https://pox.globo.com/rss/g1/economia", this.listaCategorias[2], this.listaSites[2]);
-    this.listaCadastro_Feed.push(c);
-    c = new Cadastro_Feed("http://esportes.r7.com/feed.xml", this.listaCategorias[4], this.listaSites[4]);
-    this.listaCadastro_Feed.push(c);
+    this.feedFolha("http://feeds.folha.uol.com.br/folha/")
+
+    this.feedG1("http://oglobo.globo.com/rss/", "http://pox.globo.com/rss/g1/");
+
+    this.feedUOL("http://rss.uol.com.br/feed/", "http://esporte.uol.com.br/", "http://tecnologia.uol.com.br/");
 
     localStorage.set("Cadastro_Feeds", JSON.stringify(this.listaCadastro_Feed));
   }
+  feedEstadao(url: string) {
+    //Beleza Cultura Economia Educacao Esporte Tecnologia
+    var c = new Cadastro_Feed(url+"cultura.xml", this.listaCategorias[1], this.listaSites[0]);
+    this.listaCadastro_Feed.push(c);
 
+    c = new Cadastro_Feed(url+"economia.xml", this.listaCategorias[2], this.listaSites[0]);
+    this.listaCadastro_Feed.push(c);
+
+    c = new Cadastro_Feed(url+"educacao.xml", this.listaCategorias[3], this.listaSites[0]);
+    this.listaCadastro_Feed.push(c);
+
+    c = new Cadastro_Feed(url+"esportes.xml", this.listaCategorias[4], this.listaSites[0]);
+    this.listaCadastro_Feed.push(c);
+
+    c = new Cadastro_Feed(url+"tecnologia.xml", this.listaCategorias[5], this.listaSites[0]);
+    this.listaCadastro_Feed.push(c);
+  }
+
+  feedFolha(url: string) {
+    //Beleza Cultura Economia Educacao Esporte Tecnologia
+    var c = new Cadastro_Feed(url+"dinheiro/rss091.xml", this.listaCategorias[2], this.listaSites[1]);
+    this.listaCadastro_Feed.push(c);
+
+    c = new Cadastro_Feed(url+"educacao/rss091.xml", this.listaCategorias[3], this.listaSites[1]);
+    this.listaCadastro_Feed.push(c);
+
+    c = new Cadastro_Feed(url+"informatica/rss091.xml", this.listaCategorias[5], this.listaSites[1]);
+    this.listaCadastro_Feed.push(c);
+  }
+
+  feedG1(url: string, url2:string) {
+    //Beleza Cultura Economia Educacao Esporte Tecnologia
+    var c = new Cadastro_Feed(url2+"ciencia-e-saude", this.listaCategorias[0], this.listaSites[2]);
+    this.listaCadastro_Feed.push(c);
+
+    c = new Cadastro_Feed(url+"plantaocultura.xml", this.listaCategorias[1], this.listaSites[2]);
+    this.listaCadastro_Feed.push(c);
+
+    c = new Cadastro_Feed(url2+"economia", this.listaCategorias[2], this.listaSites[2]);
+    this.listaCadastro_Feed.push(c);
+
+    c = new Cadastro_Feed(url2+"educacao", this.listaCategorias[3], this.listaSites[2]);
+    this.listaCadastro_Feed.push(c);
+
+    c = new Cadastro_Feed(url2+"esporte", this.listaCategorias[4], this.listaSites[2]);
+    this.listaCadastro_Feed.push(c);
+
+    c = new Cadastro_Feed(url2+"tecnologia", this.listaCategorias[5], this.listaSites[2]);
+    this.listaCadastro_Feed.push(c);
+
+  }
+
+  feedUOL(url: string, url2: string, url3: string) {
+    //Beleza Cultura Economia Educacao Esporte Tecnologia
+    var c = new Cadastro_Feed(url+"economia.xml", this.listaCategorias[2], this.listaSites[3]);
+    this.listaCadastro_Feed.push(c);
+
+    c = new Cadastro_Feed(url+"vestibular.xml", this.listaCategorias[3], this.listaSites[3]);
+    this.listaCadastro_Feed.push(c);
+
+    c = new Cadastro_Feed(url2+"ultimas/index.xml", this.listaCategorias[4], this.listaSites[3]);
+    this.listaCadastro_Feed.push(c);
+
+    c = new Cadastro_Feed(url3+"ultnot/index.xml", this.listaCategorias[5], this.listaSites[3]);
+    this.listaCadastro_Feed.push(c);
+  }
 }
